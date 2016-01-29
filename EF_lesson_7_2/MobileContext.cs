8b82b014e50444eadcfeae7c22fd937a7416c25e
Alex <alexis1784@gmail.com>
2016-01-29 15:17:39 +0500

    working version

---
+++ EF_lesson_7_2/MobileContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_lesson_7_2
+{
+    class MobileContext : DbContext
+    {
+        public MobileContext()
+            : base("EF_lesson_7_2")
+        { }
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<Smartphone> Smarts { get; set; }
+    }
+}
---
+++ EF_lesson_7_2/Smartphone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_lesson_7_2
+{
+    [Table("Smartphones")]
+    public class Smartphone : Phone
+    {
+        public string OS { get; set; }
+    }
+}
